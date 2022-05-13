456b497423cb4fd4b17af2ac9f501b58250fec89
Zakaria <92021986+Zaax09@users.noreply.github.com>
2022-05-13 13:51:55 +0100

    v2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,7 @@ const TodoList = ({todos, setTodos}) => {
   return (
     <div>
          {todos.map((todo) => (
-            <li className='todo-list' key={todo.id}>
+            <li className='list-item' key={todo.id}>
 
                <input 
                type="text" 
@@ -12,6 +12,19 @@ const TodoList = ({todos, setTodos}) => {
                className="list" 
                onChange={(event) => event.preventDefault()}
                />
+               <div>
+                     <button className='button-complete task-button'>
+                        <i className='fa fa-check-circle'></i>
+                     </button>
+
+                     <button className='button-edit task-button'>
+                        <i className='fa fa-edit'></i>
+                     </button>
+
+                     <button className='button-delete task-button'>
+                        <i className='fa fa-trash'></i>
+                     </button>
+               </div>
 
             </li>
          )
